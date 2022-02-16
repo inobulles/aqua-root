caf35782abf7e871a9a849e4f2b67910a0e2e58c
Obiwac <obiwac@gmail.com>
2022-02-16 11:37:36 +0100

    conf: UI R&B swizzle shader

---
+++ conf/shaders/ui/swizzle/frag.glsl
@@ -0,0 +1,16 @@
+#version 300 es
+
+precision highp float;
+
+in vec2 interp_position;
+out vec4 frag_colour;
+
+uniform float alpha;
+uniform sampler2D sampler;
+
+void main(void) {
+	vec4 colour = texture(sampler, interp_position + vec2(0.5));
+	colour.rgb /= colour.a; // undo alpha premultiplication
+
+	frag_colour = vec4(colour.rgb, alpha * colour.a);
+}
