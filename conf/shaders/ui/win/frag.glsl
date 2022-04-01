c050c92de1ced75f2b97ebe32e42eb8cd32e7474
obiwac <obiwac@gmail.com>
2022-04-01 22:03:12 +0200

    conf: Add `premul` shader for objects who's premultiplied alpha needs to be undone

--- conf/shaders/ui/win/frag.glsl
+++ conf/shaders/ui/win/frag.glsl
@@ -11,5 +11,7 @@ uniform samplerExternalOES sampler;
 
 void main(void) {
 	vec4 colour = texture2D(sampler, interp_position + vec2(0.5));
+	colour.rgb /= colour.a; // undo alpha premultiplication
+
 	frag_colour = vec4(colour.rgb, alpha * colour.a);
 }
